13b47c83828d5d0edb64661564cb4b1379840e3e
Sam-Vicera <shernandezvicera@gmail.com>
2023-10-23 14:48:10 -0700

    where I left off

--- scripts/Mortgage_Calculator.js
+++ scripts/Mortgage_Calculator.js
@@ -5,10 +5,22 @@ window.onload = init;
 
 let principal; // loan amount
 let interestRate;
-let loanLength;
-let monthlyPayments
-let totalInterestPaid
+let monthlyRate;
+let loanLength; // in years
+let totalMonths;
+
+let paymentsPerMonth;
+let totalInterestPaid; // Total money payed - Initial loan
+// Principal amount * (interest rate / 12)(1+ interest rate / 12)^ -payments
 
 function init(){
+    principal = document.getElementById("principal");
+    interestRate = document.getElementById("interest");
+    loanLength = document.getElementById("loanLength");
+    document.getElementById("submitBtn").onclick = calculate;
+}
 
+function calculate(){
+monthlyRate = interestRate/12;
+totalMonths = loanLength * 12;
 }
