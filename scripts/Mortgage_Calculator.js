8e18b92e525501002f85bb6616585f68dcf00768
Sam-Vicera <shernandezvicera@gmail.com>
2023-10-23 14:05:08 -0700

    Where I left off

---
+++ scripts/Mortgage_Calculator.js
@@ -0,0 +1,14 @@
+//  Mortgage Calculator
+window.onload = init;
+
+
+
+let principal; // loan amount
+let interestRate;
+let loanLength;
+let monthlyPayments
+let totalInterestPaid
+
+function init(){
+
+}
